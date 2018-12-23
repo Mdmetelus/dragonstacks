7581d88a0e5e15d358913ed5a767ed0c7a993f2d
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-23 15:09:41 -0500

    "nodemon added,birthdate function, dragon class made."

--- backend/dragon.js
+++ backend/dragon.js
@@ -1,8 +1,17 @@
+const DEFAULT_PROPERTIES = {
+    nickname: 'unnamed',
+    // birthdate: new Date()
+    get birthdate() {
+        return new Date();
+    }
+}
+
 class Dragon {
-    constructor(birthdate, nickname) {
-        this.birthdate = birthdate;
-        this.nickname= nickname;
+    constructor({ birthdate, nickname } = {}) {
+        //the added{} allows for avoiding the order issues that can arize in big projects.
+        this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
+        this.nickname= nickname || DEFAULT_PROPERTIES.nickname;
 
     }
 }
-module.exports= Dragons;
+module.exports = Dragon;
