4a85eb275efcaa7c6256692f127c1e0e1c9ad1a6
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-29 19:48:06 -0500

    "updates"

--- backend/app/generation/table.js
+++ backend/app/generation/table.js
@@ -3,10 +3,12 @@ const pool = require('../../databasePool');
 class GenerationTable {
     static storeGeneration(generation) {
         pool.query(
-            'INSERT INTO generation(expiration) VALUES($1)',
+            'INSERT INTO generation(expiration) VALUES($1) RETERNING id',
             [generation.expitation],
             (error, response) => {
                 if (error) return console.error(error);
+
+                const generationId = response.rows[0].id;
             }
         );
     }
