bb1f96da78e62298a4fc5b009c08e0b64a6aa7b4
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-31 23:55:48 -0500

    "dragon table promise query"

--- backend/app/dragon/table.js
+++ backend/app/dragon/table.js
@@ -2,6 +2,18 @@ const pool = require('../../databasePool');
 class DragonTable {
     static storeDragon (dragon) {
         const { birthdate, nickname, generationId } = dragon;
+
+        return new Promise((resolve, reject) => {
+            pool.query(
+                `INSERT INTO dragon(birthdate, nickname, "generationId") 
+                VALUES($1, $2, $3) RETURNING id`,
+                [birthdate, nickname, generationId],
+                (error, response) => {
+                    if(error) return reject(error);
+
+                }
+            )
+        })
     }
 }
 
